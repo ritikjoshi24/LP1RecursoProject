e82b21f62227d399787ab21c365260fb38e913d3
Guilherme-21903863 <Paulo.Style222@gmail.com>
2021-06-21 00:16:35 +0100

    Modified Card and Deck class

--- Project/Card.cs
+++ Project/Card.cs
@@ -10,12 +10,12 @@ namespace Project
 
         public enum COLORCARD
         {
-            GREEN, YELLOW, WILD, RED, BLUE
+            GREEN, YELLOW, WILDCOLOR, RED, BLUE
         }
 
         public enum VALUE
         {
-            ZERO = '0', ONE = '1', TWO = '2', THREE = '3', FOUR = '4', FIVE = '5', SIX = '6', SEVEN = '7', EIGHT = '8', NINE = '9', DRAWTWO = 'b', DRAWFOUR = 'a', SKIP = 'c', WILD = 'd', REVERSE = 'e'
+            ZERO = '0', ONE = '1', TWO = '2', THREE = '3', FOUR = '4', FIVE = '5', SIX = '6', SEVEN = '7', EIGHT = '8', NINE = '9', DRAWTWO = 'b', DRAWFOUR = 'a', SKIP = 'c', WILD = 'd', REVERSE = 'e', EMPTY = 'm'
         }
 
         public COLORCARD mycard { get; set; }
--- Project/DeckOfCards.cs
+++ Project/DeckOfCards.cs
@@ -2,91 +2,80 @@ using System;
 using System.Collections.Generic;
 namespace Project
 {
-    public class DeckOfCards
+    public class DeckOfCards : Card
     {
-        public List<Card> cards { get; set; }
+        const long NUM_OF_CARDS = 112;
+        private Card[] deck;
+        public DeckOfCards()
+        {
+            deck = new Card[NUM_OF_CARDS];
+        }
+        public Card[] getDeck { get { return deck; } }
         public void CardDeck()
         {
-            cards = new List<Card>();
+            long i = 0;
 
-            foreach (Card.COLORCARD color in Enum.GetValues(typeof(Card.COLORCARD)))
+            foreach (COLORCARD color in Enum.GetValues(typeof(COLORCARD)))
             {
-                if (color != Card.COLORCARD.WILD)
+                if (color != COLORCARD.WILDCOLOR)
                 {
-                    foreach (Card.VALUE value in Enum.GetValues(typeof(Card.VALUE)))
+                    foreach (VALUE value in Enum.GetValues(typeof(VALUE)))
                     {
                         switch (value)
                         {
-                            case Card.VALUE.ONE:
-                            case Card.VALUE.THREE:
-                            case Card.VALUE.TWO:
-                            case Card.VALUE.FOUR:
-                            case Card.VALUE.SIX:
-                            case Card.VALUE.NINE:
-                            case Card.VALUE.SEVEN:
-                            case Card.VALUE.FIVE:
-                            case Card.VALUE.EIGHT:
-                            case Card.VALUE.REVERSE:
-                            case Card.VALUE.DRAWTWO:
-                            case Card.VALUE.SKIP:
-                                cards.Add(new Card()
-                                {
-                                    mycard = color,
-                                    myValue = value
-                                });
-                                cards.Add(new Card()
-                                {
-                                    mycard = color,
-                                    myValue = value
-                                });
+                            case VALUE.ONE:
+                            case VALUE.THREE:
+                            case VALUE.TWO:
+                            case VALUE.FOUR:
+                            case VALUE.SIX:
+                            case VALUE.NINE:
+                            case VALUE.SEVEN:
+                            case VALUE.FIVE:
+                            case VALUE.EIGHT:
+                            case VALUE.REVERSE:
+                            case VALUE.DRAWTWO:
+                            case VALUE.SKIP:
+                                deck[i] = new Card { mycard = color, myValue = value };
+                                i++;
+                                deck[i] = new Card { mycard = color, myValue = value };
+                                i++;
                                 break;
 
-                            case Card.VALUE.ZERO:
-                                cards.Add(new Card()
-                                {
-                                    mycard = color,
-                                    myValue = value
-                                });
+                            case VALUE.ZERO:
+                                deck[i] = new Card { mycard = color, myValue = value };
+                                i++;
+                                deck[i] = new Card { mycard = color, myValue = VALUE.EMPTY };
+                                i++;
                                 break;
                         }
                     }
                 }
                 else
                 {
-                    for (long i = 1; i <= 4; i++)
+                    for (long j = 1; j <= 4; j++)
                     {
-                        cards.Add(new Card()
-                        {
-                            mycard = color,
-                            myValue = Card.VALUE.DRAWFOUR
-                        });
-                        cards.Add(new Card()
-                        {
-                            mycard = color,
-                            myValue = Card.VALUE.WILD
-                        });
+                        deck[i] = new Card { mycard = color, myValue = VALUE.DRAWFOUR };
+                        i++;
+                        deck[i] = new Card { mycard = color, myValue = VALUE.WILD };
+                        i++;
                     }
                 }
-                CardShuffle();
             }
 
+            CardShuffle();
         }
         public void CardShuffle()
         {
             Random rand = new Random();
-            List<Card> card = cards;
             Card temp;
-
-            for (long shuffleTimes = 0; shuffleTimes < 1000; shuffleTimes++)
+            for (int k = 0; k < NUM_OF_CARDS; k++)
             {
-                for (int j = card.Count - 1; j > 0; --j)
-                {
-                    int secondCardIndex = rand.Next(j + 1);
-                    temp = card[j];
-                    card[j] = card[secondCardIndex];
-                    card[secondCardIndex] = temp;
-                }
+                int secondCardIndex = rand.Next(k + 1);
+                temp = deck[k];
+                deck[k] = deck[secondCardIndex];
+                deck[secondCardIndex] = temp;
             }
         }
+
     }
 }
